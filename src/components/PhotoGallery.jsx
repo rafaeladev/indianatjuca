1a2e6938057057dc1bd686c51bd3773d32d12637
Rafaela <104620799+rafaeladev@users.noreply.github.com>
2023-08-07 15:37:39 +0200

    gallery

--- src/components/PhotoGallery.jsx
+++ src/components/PhotoGallery.jsx
@@ -1,44 +1,58 @@
 import { nanoid } from 'nanoid';
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const PhotoGallery = (props) => {
     const horizontalPics = props.data.filter((pic) => pic.position === 'horizontal');
     const verticalPics = props.data.filter((pic) => pic.position === 'vertical');
-    const photosH = horizontalPics.map((photo) => {
-        return (
-            <Link
-                to={'.'}
-                key={nanoid()}
-                className={`photoGallery__horizontal`}
-            >
-                <img
-                    src={`natal/natal${props.year}/${photo.name}.${props.format}`}
-                    alt={`${photo.id}`}
-                    className='galleryImg'
-                />
-            </Link>
-        );
-    });
+    const [file, setFile] = useState(null);
+    // const photosH = horizontalPics.map((photo, index) => {
+    //     return (
+    //         <div
+    //             key={nanoid()}
+    //             className={`photoGallery__horizontal`}
+    //             onClick={() => setFile(photo)}
+    //         >
+    //             <img
+    //                 src={`natal/natal${props.year}/${photo.name}.${props.format}`}
+    //                 alt={`${photo.id}`}
+    //                 className='galleryImg'
+    //             />
+    //         </div>
+    //     );
+    // });
     const photosV = verticalPics.map((photo) => {
         return (
-            <Link
-                to={'.'}
-                key={nanoid()}
-                className={`photoGallery__vertical`}
-            >
-                <img
-                    src={`natal/natal${props.year}/${photo.name}.${props.format}`}
-                    alt={`${photo.id}`}
-                    className='galleryImg'
-                />
-            </Link>
+            <>
+                <div
+                    key={nanoid()}
+                    className={`photoGallery__vertical`}
+                    onClick={() => setFile(photo.name)}
+                >
+                    <img
+                        src={`natal/natal${props.year}/${photo.name}.${props.format}`}
+                        alt={`${photo.id}`}
+                        className='galleryImg'
+                    />
+                </div>
+                <div
+                    className='popup-media'
+                    style={{ display: file ? 'block' : 'none' }}
+                >
+                    <span onClick={() => setFile(null)}>&times;</span>
+                    <img
+                        src={`natal/natal${props.year}/${file}.${props.format}`}
+                        alt={`${photo.id}`}
+                        className='galleryImg'
+                    />
+                </div>
+            </>
         );
     });
     return (
         <>
             {verticalPics && <div className='container container--vertical'>{photosV}</div>}
-            {horizontalPics && <div className='container container--horizontal'>{photosH}</div>}
+            {/* {horizontalPics && <div className='container container--horizontal'>{photosH}</div>} */}
         </>
     );
 };
