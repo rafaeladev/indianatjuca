e23fbb614f5170199088aa9b0822653ce0937622
Rafaela <104620799+rafaeladev@users.noreply.github.com>
2023-11-29 15:43:27 +0100

    test : without content

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -107,11 +107,11 @@ const Home = () => {
                             {homeTitle}
                         </h2>
                     )} */}
-                    {content && (
+                    {/* {content && (
                         <div className='homeContent__text homeContent--margin'>
                             <p className='text-fade'>{content}</p>
                         </div>
-                    )}
+                    )} */}
                     <div className='homeContent__actions '>
                         <Link to={`natal/${years[yearsSize - 1]}`}>
                             <button className='buttonNow'>{subtitles.action}</button>
